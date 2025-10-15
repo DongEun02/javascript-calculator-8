c5bfda60c5f3a73914b9f4a50bebd75c9ce2e6d3
김동은 <ehddms0517@naver.com>
2025-10-15 15:48:34 +0900

    fix(util): 테스트코드 출력 형식에 맞춰 공백 추가

--- src/util.js
+++ src/util.js
@@ -1,13 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export const readInput = async () => {
-  Console.print('덧셈할 문자열을 입력해 주세요.');
-  const input = await Console.readLineAsync('');
+  const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
   return input;
 };
 
 export const printResult = (result) => {
-  Console.print(`결과: ${result}`);
+  Console.print(`결과 : ${result}`);
 };
 
 export const separateNumbers = (input) => {
