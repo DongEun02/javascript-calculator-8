cd372f5fcd73bbcad29abf3067ffb753593d40c9
김동은 <ehddms0517@naver.com>
2025-10-17 20:36:39 +0900

    fix(util): 사용자 입력 줄바꿈 추가

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export const readInput = async () => {
-  const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+  const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
   return input;
 };
 
